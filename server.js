786aecc7769b433ab5ae043d797df5d99a916150
GregorickLiriano <gregorickliriano@gmail.com>
2026-09-14 00:22:45 -0400

    server.js path uri fixed

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
+    // Passenger strips /demo, we prepend it back for Next.js basePath
+    if (!req.url.startsWith('/demo')) {
+      req.url = '/demo' + req.url;
+    }
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
   }).listen(process.env.PORT || 3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:' + (process.env.PORT || 3000))
-  })
-})
+    if (err) throw err;
+    console.log('> Ready on port ' + (process.env.PORT || 3000));
+  });
+});
